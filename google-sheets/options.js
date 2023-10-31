4e10275fe439c99cb0bae044ec1737ab3118386f
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2023-10-31 21:54:36 +0530

    excel clone options integration

--- google-sheets/options.js
+++ google-sheets/options.js
@@ -1,6 +1,7 @@
 const cellNamePlaceholder = document.querySelector("#active-cell");
 const fontSizeInput = document.querySelector("#fontsize");
 const fontFamilyInput = document.querySelector("#fontfamily");
+const form = document.querySelector("#form")
 
 let activeElement = null;
 
@@ -9,9 +10,9 @@ const state = {};
 const defaultProperties = {
     fontFamily: 'sans',
     fontSize: 16,
-    color: "#000",
+    color: "#000000",
     textAlign: "left", // "left", "center", "right"
-    backgroundColor: "#fff",
+    backgroundColor: "#ffffff",
     isBold: false,
     isItalic: false,
     isUnderlined: false
@@ -43,17 +44,59 @@ function resetOptions(optionsState) {
      *    textAlign: ''
      * }
      */
-    fontSizeInput.value = optionsState.fontSize;
-    fontFamilyInput.value = optionsState.fontFamily;
+    form.fontfamily.value = optionsState.fontFamily;
+    form.fontsize.value = optionsState.fontSize;
+    form.textalign.value = optionsState.textAlign; // "right" | "left" | "center"
+    form.bold.checked = optionsState.isBold
+    form.italic.checked = optionsState.isItalic;
+    form.underlined.checked = optionsState.isUnderlined;
+    form.textcolor.value = optionsState.color;
+    form.bgcolor.value = optionsState.backgroundColor;
 }
 
-// resetOptions({
-//     fontFamily: 'monospace',
-//     fontSize: 19,
-//     color: "#000",
-//     textAlign: "left", // "left", "center", "right"
-//     backgroundColor: "#fff",
-//     isBold: false,
-//     isItalic: false,
-//     isUnderlined: false
-// })
+function onFormChange() {
+    if (!activeElement) {
+        alert("Please select a cell to make changes");
+        form.reset();
+        return;
+    }
+
+
+    let currentState = {
+        textColor: form.textcolor.value,
+        backgroundColor: form.bgcolor.value,
+        fontSize: form.fontsize.value,
+        fontFamily: form.fontfamily.value,
+        isBold: form.bold.checked,
+        isItalic: form.italic.checked,
+        isUnderlined: form.underlined.checked,
+        textAlign: form.textalign.value // "left" , "right" , "center"
+    }
+
+    // below function applies all the styles to the active cell.
+    applyStylesToCell(currentState);
+
+    // update the state object for the current cell.
+    // state = {} 
+    // state["C2"] = currentState ;
+    // state = { C2: currentState }
+    state[activeElement.id] = currentState;
+}
+
+function applyStylesToCell(styleObject) {
+    // it will take the style object and applies it to the currently selected cell.
+    activeElement.style.fontSize = `${styleObject.fontSize}px`;
+    activeElement.style.fontFamily = styleObject.fontFamily;
+    activeElement.style.color = styleObject.textColor;
+    activeElement.style.backgroundColor = styleObject.backgroundColor;
+    activeElement.style.textAlign = styleObject.textAlign;
+    if (styleObject.isBold) {
+        activeElement.style.fontWeight = "bold";
+    }
+    if (styleObject.isItalic) {
+        activeElement.style.fontStyle = "italic";
+    }
+    if (styleObject.isUnderlined) {
+        activeElement.style.textDecoration = "underline";
+    }
+}
